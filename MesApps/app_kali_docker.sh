c00a9a8bfd4c58f30cfae1bdf60d312c4d9b99af
DOS SANTOS <danielitto91@gmail.com>
2019-12-26 08:54:45 +0100

    Update app_kali_docker.sh

--- MesApps/app_kali_docker.sh
+++ MesApps/app_kali_docker.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 # init
-apt-get clean 
-apt-get update 
-apt-get upgrade -y 
-apt-get dist-upgrade -y
+apt clean 
+apt update 
+apt upgrade -y 
+apt full-upgrade -y
 
 # variables
-usertos=$(w | awk '{print $1}' | awk 'NR==3')
-interfacewifi=$(ip link | grep ^3 | awk '{print $2}' | sed s'/://')
+usertos="root"
 interfacenet=$(ip link | grep ^2 | awk '{print $2}' | sed s'/://')
-liste=$(apt list | grep libreoffice-l10n-* | awk -F'/' '{print $1}')
 versiontor=9.0.1
 
 # installation de simplenote
@@ -26,15 +24,6 @@ usermod -a -G wireshark "$usertos"
 chgrp wireshark /usr/bin/dumpcap
 chmod 771 /usr/bin/dumpcap
 setcap cap_net_raw,cap_net_admin=eip /usr/bin/dumpcap
-ip link set "$interfacenet" promisc on
-ip link set "$interfacewifi" promisc on
-
-# Installation d'AnyDesk
-wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
-echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
-sleep 1
-apt update
-apt install anydesk -y
 
 # Installation de Visual Studio Code
 wget http://packages.microsoft.com/repos/vscode/pool/main/c/code/code_1.40.1-1573664190_amd64.deb
@@ -50,11 +39,11 @@ cp katoolin/katoolin.py /usr/bin/katoolin
 chmod +x /usr/bin/katoolin
 rm -rf katoolin
 
-# App de sécurité
+# App de secu
 apt install tor -y
 service tor start
-apt install zenmap -y
-apt-get install htop nethogs -y
+apt install htop -y 
+apt install nethogs -y
 apt install maltego -y
 apt install metasploit-framework -y
 apt install burpsuite -y
@@ -67,24 +56,22 @@ apt install nikto -y
 apt install psmisc -y
 apt install openvas -y
 # Installation de tor
-#mkdir -p /root/Documents/TOR/
-#cd /root/Documents/TOR/
-#lynx -dump https://dist.torproject.org/torbrowser | sed "13 q"
+mkdir -p /root/Documents/TOR/
+cd /root/Documents/TOR/
+lynx -dump https://dist.torproject.org/torbrowser | sed "13 q"
 # Téléchargement de tor
-#wget https://dist.torproject.org/torbrowser/"$versiontor"/tor-browser-linux64-"$versiontor"_fr.tar.xz   
+wget https://dist.torproject.org/torbrowser/"$versiontor"/tor-browser-linux64-"$versiontor"_fr.tar.xz   
 # Extratction
-#tar xvf tor-browser-linux64*  
-#cd /root/Documents/TOR/tor-browser_fr/
+tar xvf tor-browser-linux64*  
+cd /root/Documents/TOR/tor-browser_fr/
 # Création du raccourci
-#./start-tor-browser.desktop --register-app
+./start-tor-browser.desktop --register-app
 # Bug root
-#if [ "$(id -u)" -eq 0 ]; then
-#           sed -i -e 's/-eq 0/-eq 1000/' /root/Documents/TOR/tor-browser_fr/Browser/start-tor-browser 
-#fi
+if [ "$(id -u)" -eq 0 ]; then
+           sed -i -e 's/-eq 0/-eq 1000/' /root/Documents/TOR/tor-browser_fr/Browser/start-tor-browser 
+fi
 
-#rm -rf /root/Documents/TOR/*_fr.tar.xz
-
-# nettoyage du système
+rm -rf /root/Documents/TOR/*_fr.tar.xz
 
 # Autres programmes
 apt install vlc -y
@@ -98,17 +85,13 @@ apt install keepassx -y
 apt install remmina -y
 apt install terminator -y
 apt install nwipe -y
-apt install fslint -y
 apt install lynx -y
-apt install software-center -y
 apt install alsa-utils -y
-apt install gtk-recordmydesktop -y
-apt install recordmydesktop -y
 apt install synaptic -y
 apt install tree -y
 apt install screen -y
 apt install vim -y
 
-# nétoyage
+# clean
 apt autoremove --purge -y
 apt autoclean
