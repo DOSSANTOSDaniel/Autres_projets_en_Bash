f97360aa15a8b0fb7658533012590be8e1b6c2f8
DOS SANTOS <dossantosjdf@gmail.com>
2022-05-04 12:49:20 +0200

    Update installminidlna.sh

--- DLNA/installminidlna.sh
+++ DLNA/installminidlna.sh
@@ -10,44 +10,69 @@ images="PHOTO"
 musiques="AUDIO"
 books="BOOK"
 
-apt install toilet -y > /dev/null
+sudo apt install toilet -y > /dev/null
 sleep 1
 clear
 toilet -f smblock --filter metal 'MiniDLNA'
 echo -e "\n"
 
-apt update
-apt full-upgrade -y
-apt install ffmpeg -y
-apt install libdlna0 -y
-apt install pulseaudio-dlna -y
-apt install minidlna -y
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt install ffmpeg -y
+sudo apt install libdlna0 -y
+sudo apt install pulseaudio-dlna -y
+sudo apt install minidlna -y
 
-usermod -G minidlna $userdlna
+sudo usermod -a -G minidlna $userdlna
 
-mkdir -p /home/Partage/{PHOTO,VIDEO,AUDIO,BOOK}
+mkdir -p /home/Partage/{PHOTO,VIDEO,AUDIO}
 sleep 1
 mkdir /var/log/dlna
+sudo chown -R minidlna:minidlna /var/log/dlna
 sleep 1
 
 echo "
 user=$userdlna
+
 media_dir=V,${pathdlna}/${videos}
 media_dir=P,${pathdlna}/${images}
 media_dir=A,${pathdlna}/${musiques}
+
 db_dir=/var/cache/minidlna
+
 log_dir=/var/log/dlna
+
+#Définir une racine de l'arborescence des répertoires présentée sur les machines clientes
+#   * "." - standard container
+#   * "B" - "Browse Directory"
+#   * "M" - "Music"
+#   * "P" - "Pictures"
+#   * "V" - "Video"
+root_container=B
+
 port=8200
+
 network_interface=$intnet
+
+# Nom réseau
 friendly_name=$banniere
+
+# Détection automatique des nouveaux fichiers.
 inotify=yes
+
 album_art_names=Cover.jpg/cover.jpg/AlbumArtSmall.jpg/albumartsmall.jpg
 album_art_names=AlbumArt.jpg/albumart.jpg/Album.jpg/album.jpg
 album_art_names=Folder.jpg/folder.jpg/Thumb.jpg/thumb.jpg
+
 enable_tivo=no
+
 strict_dlna=no
+
+#Délai de mise à jour de l'indexation des fichiers
 notify_interval=30
+
 max_connections=5
+
 " > /etc/minidlna.conf
 
 sleep 2
@@ -62,3 +87,4 @@ clear
 
 ss -lntp
 
+# minidlna -R ---> Scanner et indexer les fichiers multimédia dans la base du serveur UPNP/DLNA
